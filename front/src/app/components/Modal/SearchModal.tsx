6932a66ee4d2c9a16680896e7f5982bac05d4e48
brgndy <brgndy96@gmail.com>
2023-04-04 20:00:33 +0900

    msg

--- front/src/app/components/Modal/SearchModal.tsx
+++ front/src/app/components/Modal/SearchModal.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import classes from "./SearchModal.module.css";
 import BackDrop from "./BackDrop";
 
@@ -11,26 +10,12 @@ export default function SearchModal({
   modalIsOpen,
   modalCloseHandler,
 }: modalOpen) {
-  const [isVisible, setIsVisible] = useState(false);
-
-  useEffect(() => {
-    if (modalIsOpen) {
-      setIsVisible(true);
-    } else {
-      const timeout = setTimeout(() => {
-        setIsVisible(false);
-      }, 500);
-
-      return () => clearTimeout(timeout);
-    }
-  }, [modalIsOpen]);
-
   return (
     <>
       <BackDrop modalCloseHandler={modalCloseHandler}>
         <div
           className={`${classes.search_modal_cotainer} ${
-            isVisible ? (modalIsOpen ? classes.show : classes.hide) : ""
+            modalIsOpen ? classes.show : classes.hide
           }`}
         >
           모달내용
