84ae9baa3416171171b9049f2923846c248a3f13
brgndy <brgndy96@gmail.com>
2023-04-05 17:38:26 +0900

    msg

---
+++ front/src/app/components/Modal/SignUpModal/SignUpModal.tsx
@@ -0,0 +1,12 @@
+import classes from "./SignUpModal.module.css";
+import IsNotLoginBackDrop from "../IsNotLogin/IsNotLoginBackDrop";
+
+export default function SignUpModal() {
+  return (
+    <>
+      <IsNotLoginBackDrop>
+        <div>gkdl</div>
+      </IsNotLoginBackDrop>
+    </>
+  );
+}
--- front/src/app/components/Modal/isNotLogin/IsNotLoginBackDrop.tsx
+++ front/src/app/components/Modal/isNotLogin/IsNotLoginBackDrop.tsx
@@ -1,10 +1,21 @@
+"use client";
 import classes from "./IsNotLoginBackdrop.module.css";
+import { useEffect } from "react";
+import { preventScroll, allowScroll } from "@/utils/modal";
 
 type Children = {
   children: React.ReactNode;
 };
 
 export default function IsNotLoginBackDrop({ children }: Children) {
+  useEffect(() => {
+    const preventScrollY = preventScroll();
+
+    return () => {
+      allowScroll(preventScrollY);
+    };
+  }, []);
+
   return (
     <>
       <div className={classes.backdrop}>{children}</div>
