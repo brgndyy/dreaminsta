13a509fe07b71a0def7a26b8d68924ee50c75c17
brgndy <brgndy96@gmail.com>
2023-04-05 17:38:18 +0900

    msg

--- front/src/app/components/Modal/isNotLogin/isNotLogin.tsx
+++ front/src/app/components/Modal/isNotLogin/isNotLogin.tsx
@@ -1,20 +1,9 @@
-"use client";
-
 import classes from "./isNotLogin.module.css";
 import IsNotLoginBackDrop from "./IsNotLoginBackDrop";
-import { preventScroll, allowScroll } from "@/app/utils/modal";
-import { useEffect } from "react";
+
 import Link from "next/link";
 
 export default function IsNotLogin() {
-  useEffect(() => {
-    const prevScrollY = preventScroll();
-
-    return () => {
-      allowScroll(prevScrollY);
-    };
-  }, []);
-
   return (
     <>
       <IsNotLoginBackDrop>
