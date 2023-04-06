99209ce8fba1a1e4ead4cf70b6eacdb91eb145bc
brgndy <brgndy96@gmail.com>
2023-04-06 13:34:48 +0900

    msg

--- front/src/app/components/Modal/SignUpModal/SignUpModal.tsx
+++ front/src/app/components/Modal/SignUpModal/SignUpModal.tsx
@@ -1,11 +1,100 @@
+"use client";
+
 import classes from "./SignUpModal.module.css";
 import IsNotLoginBackDrop from "../IsNotLogin/IsNotLoginBackDrop";
+import { useState, useRef, useEffect } from "react";
+
+type SignUp = {
+  email: string;
+  nick: string;
+  password: string;
+};
 
 export default function SignUpModal() {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const nickRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const [signUpForm, setSignUpForm] = useState({});
+
+  const formSubmitHandler = async (e: React.MouseEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (emailRef.current && nickRef.current && passwordRef.current) {
+      const email = emailRef.current.value;
+      const nick = nickRef.current.value;
+      const password = passwordRef.current.value;
+
+      setSignUpForm({ email, nick, password });
+
+      await sendForm({ email, nick, password });
+
+      emailRef.current.value = "";
+      nickRef.current.value = "";
+      passwordRef.current.value = "";
+    }
+  };
+
+  const sendForm = async ({ email, nick, password }: SignUp) => {
+    const res = await fetch("http://localhost3002/api/user/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        nick: nick,
+        password: password,
+      }),
+    });
+
+    if (!res.ok) {
+      throw new Error("에러가 발생했습니다!");
+    }
+
+    const data = await res.json();
+
+    console.log(data);
+  };
+
   return (
     <>
       <IsNotLoginBackDrop>
-        <h1>회원가입</h1>
+        <div className={classes.signup_container}>
+          <div className={classes.logo_banner}>
+            <h2>회원가입</h2>
+          </div>
+          <form
+            method="POST"
+            className={classes.signup_form}
+            onSubmit={formSubmitHandler}
+          >
+            <div className={classes.section}>
+              <div className={classes.label}>
+                <label htmlFor="email">이메일</label>
+              </div>
+              <div className={classes.input}>
+                <input ref={emailRef} type="text" name="email" />
+              </div>
+            </div>
+            <div className={classes.section}>
+              <div className={classes.label}>
+                <label htmlFor="nick">닉네임</label>
+              </div>
+              <div className={classes.input}>
+                <input ref={nickRef} type="text" name="nick" />
+              </div>
+            </div>
+            <div className={classes.section}>
+              <div className={classes.label}>
+                <label htmlFor="password">비밀번호</label>
+              </div>
+              <div className={classes.input}>
+                <input ref={passwordRef} type="password" name="password" />
+              </div>
+            </div>
+            <button className={classes.form_button}>가입</button>
+          </form>
+        </div>
       </IsNotLoginBackDrop>
     </>
   );
